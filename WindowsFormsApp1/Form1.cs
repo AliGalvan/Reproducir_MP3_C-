cd04368e21a1f81e6c4ec6107be44a826e74c6e1
Ali Galvan <65520492+AliGalvan@users.noreply.github.com>
2020-05-17 22:09:43 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        WindowsMediaPlayer sonido;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                if (sonido == null)
+                {
+                    sonido = new WindowsMediaPlayer();
+                    sonido.URL = @"C:\";
+                    sonido.controls.play();
+                    sonido.settings.setMode("loop", true);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No hay cancion");
+            }
+
+        }
+
+        
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sonido == null)
+                {
+                    sonido = new WindowsMediaPlayer();
+                    sonido.URL = @"C:\Users\aliga\Music\Buttercup.mp3";
+                    sonido.controls.play();
+                    sonido.settings.setMode("loop", true);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No hay cancion");
+            }
+
+        }*/
+
+    }
+}
